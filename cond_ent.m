df30747f272681d3e7a6454dae4079e8d762e5e6
Yunus Emre Koç <yunusemre.koc@outlook.com>
2017-08-16 18:57:51 +0300

    Add files via upload

---
+++ cond_ent.m
@@ -0,0 +1,12 @@
+function result = cond_ent(Y, X)
+
+result = 0;
+tab = tabulate(X);
+tab = tab(tab(:,3)~=0,:);
+
+for i = 1:size(tab,1)
+   
+    H = ent(Y(X == tab(i,1)));
+    prob = tab(i, 3) / 100;
+    result = result + prob * H;
+end
---
+++ ent.m
@@ -0,0 +1,5 @@
+function result = ent(Y)
+tab = tabulate(Y);
+prob = tab(:,3) / 100;
+prob = prob(prob~=0);
+result = -sum(prob .* log2(prob));
